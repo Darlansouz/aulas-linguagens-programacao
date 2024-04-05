d124da1afc5addb3f6ceb44738f2cea22316e2fd
Kelvin Bentes <kelvinbentes20@gmail.com>
2024-04-05 20:45:00 -0300

    fizemos um programa de idade e altura

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using  namespace std;
+
+int main(){
+
+    int idade = 0;
+    float altura;
+    cout << "Digite sua idade" << endl;
+    cin >> idade;
+    cout << " a idade e:" << idade << endl;
+    cin >> altura;
+    cout << "a altura e:" << altura << endl;
+
+
+
+
+
+    return 0;
+}
